f841ced0af1191478c2935794315439aa85c6f3c
tuyenmx <tuyenmx@ominext.com>
2019-10-23 13:38:03 +0700

    first commit thienhd

---
+++ api_laravel_angular/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Employee::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        return Employee::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Employee::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  $id)
+    {
+        //
+        $employee = Employee::findOrFail($id);
+        $employee->update($request->all());
+
+        return $employee;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(Request $request,  $id)
+    {
+        //
+        $employee = Employee::findOrFail($id);
+        $employee->delete();
+
+        return 204;
+
+    }
+}
---
+++ api_laravel_angular/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('employees', 'EmployeeController@index');
+Route::get('employees/{id}', 'EmployeeController@show');
+Route::post('employees', 'EmployeeController@store');
+Route::put('employees/{id}', 'EmployeeController@update');
+Route::delete('employees/{id}', 'EmployeeController@delete');
